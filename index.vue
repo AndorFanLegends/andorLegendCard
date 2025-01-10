2fb9f3517db6d0e46c5694e5a6674d4fd6eb2a89
dchansel <damien@13ruedebrioiux.com>
2025-01-10 15:08:11 +0100

    try add 2nd class

--- index.vue
+++ index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card" :class="{ 'print': cardType === 'print', 'app': cardType === 'app' }">
-    <div class="left {{ theme }}" :class="{ 'left_letter': cardData.type === 'letter' || cardData.type === 'end' }" v-if="!(cardType === 'app')">
+    <div :class="left {theme}" :class="{ 'left_letter': cardData.type === 'letter' || cardData.type === 'end' }" v-if="!(cardType === 'app')">
       <!--<div class="left_number">{{ number }}</div>
       <div class="right_number">{{ number }}</div>-->
       <div class="legend">
